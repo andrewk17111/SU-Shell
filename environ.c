151c3a6a38c567ae75046d15a68f7a02e3aeefa6
ak8072 <ak8072 [at] engr.ship.edu>
2021-03-23 01:06:25 -0400

    Added comments for environ

--- environ.c
+++ environ.c
@@ -7,6 +7,15 @@
 
 LIST_HEAD(environment);
 
+/**
+ * Returns a string that starts at the given index at the length given
+ * 
+ * @param str - The source string you want to get a part of
+ * @param start - The starting index of the substring
+ * @param length - The length of the substring
+ * 
+ * @return extracted substring
+ **/
 char *environ_sub_string(char* str, int start, int length) {
     char* output = calloc(length + 1, sizeof(char));
     for (int i = start; i < (start + length) && i < strlen(str); i++) {
@@ -15,6 +24,13 @@ char *environ_sub_string(char* str, int start, int length) {
     return output;
 }
 
+/**
+ * Returns a string array of the name and value for an evironment variable
+ * 
+ * @param env_var_str - Environment variable string
+ * 
+ * @return String array for name and value of environ var
+ **/
 char **split_environ_var(char *env_var_str) {
     int env_var_len = strlen(env_var_str);
     char **env_var_arr = calloc(2, sizeof(char *));
@@ -30,6 +46,11 @@ char **split_environ_var(char *env_var_str) {
     return env_var_arr;
 }
 
+/**
+ * Returns a string array for the environment variables from the environment list
+ * 
+ * @return Environment string array
+ **/
 char **make_environ() {
     char **envp = calloc(list_size(&environment) + 1, sizeof(char *));
     struct list_head *head = &environment;
@@ -47,6 +68,11 @@ char **make_environ() {
     return envp;
 }
 
+/**
+ * Sets up the environment linked list
+ * 
+ * @param envp - Environment string array like envp from main
+ **/
 void environ_init(char **envp) {
     for (int i = 0; envp[i] != NULL; i++) {
         struct environ_var_t *var = malloc(sizeof(struct environ_var_t));
@@ -58,28 +84,54 @@ void environ_init(char **envp) {
     environ_set_var("PS1", "~");
 }
 
-bool environ_var_exist(char *var_name) {
+/**
+ * Checks if an environment variable exists
+ * 
+ * @param name - Environment variable name
+ * 
+ * @return true if variable exists in the environment; false if it doesn't
+ **/
+bool environ_var_exist(char *name) {
     struct list_head *head = &environment;
     struct list_head *curr;
     struct environ_var_t *env_var;
     for (curr = head->next; curr != head; curr = curr->next) {
         env_var = list_entry(curr, struct environ_var_t, list);
-        if (strcmp(env_var->name, var_name) == 0)
+        if (strcmp(env_var->name, name) == 0)
             return true;
     }
     return false;
 }
 
+/**
+ * Add an environment variable struct to the linked list
+ * 
+ * @param name - Name of the new environment variable
+ * @param value - Value of the new environment variable
+ **/
 void environ_add_var(char *name, char *value) {
     struct environ_var_t var = { .name = name, .value = value };
     list_add_tail(&var.list, &environment);
 }
 
+/**
+ * Update an existing environment variable struct in the linked list
+ * 
+ * @param name - Name of the environment variable
+ * @param value - Value of the environment variable
+ **/
 void environ_update_var(char *name, char *value) {
     struct environ_var_t *var = environ_get_var(name);
     var->value = value;
 }
 
+/**
+ * Add new environment variable if one of the given name
+ * doesn't exist or update it if one does exist
+ * 
+ * @param name - Name of the environment variable
+ * @param value - Value of the environment variable
+ **/
 void environ_set_var(char *name, char *value) {
     if (environ_var_exist(name))
         environ_update_var(name, value);
@@ -87,12 +139,28 @@ void environ_set_var(char *name, char *value) {
         environ_add_var(name, value);
 }
 
+/**
+ * Remove an environment variable of the given name
+ * if it is in the list
+ * 
+ * @param name - Name of the environment variable
+ **/
 void environ_remove_var(char *name) {
     struct environ_var_t *var = environ_get_var(name);
-    if (var != NULL)
+    if (var != NULL) {
         list_del(&var->list);
+        free (var);
+    }
 }
 
+/**
+ * Returns the environment variable that has the
+ * given name
+ * 
+ * @param name - Name of the environment variable
+ * 
+ * @return The environment variable
+ **/
 struct environ_var_t *environ_get_var(char *name) {
     if (environ_var_exist(name)) {
         struct list_head *head = &environment;
--- environ.h
+++ environ.h
@@ -11,11 +11,49 @@ struct environ_var_t {
     struct list_head list;
 };
 
+/**
+ * Returns a string array for the environment variables from the environment list
+ * 
+ * @return Environment string array
+ **/
 char **make_environ();
+/**
+ * Sets up the environment linked list
+ * 
+ * @param envp - Environment string array like envp from main
+ **/
 void environ_init(char **envp);
-bool environ_var_exist(char *var_name);
+/**
+ * Checks if an environment variable exists
+ * 
+ * @param name - Environment variable name
+ * 
+ * @return true if variable exists in the environment; false if it doesn't
+ **/
+bool environ_var_exist(char *name);
+/**
+ * Add new environment variable if one of the given name
+ * doesn't exist or update it if one does exist
+ * 
+ * @param name - Name of the environment variable
+ * @param value - Value of the environment variable
+ **/
 void environ_set_var(char *name, char *value);
+/**
+ * Remove an environment variable of the given name
+ * if it is in the list
+ * 
+ * @param name - Name of the environment variable
+ **/
 void environ_remove_var(char *name);
-struct environ_var_t *environ_get_var(char *var_name);
-
+/**
+ * Returns the environment variable that has the
+ * given name
+ * 
+ * @param name - Name of the environment variable
+ * 
+ * @return The environment variable
+ **/
+struct environ_var_t *environ_get_var(char *name);
+//environ_cleanup
 #endif
