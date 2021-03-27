fb839200466f45660396b33f7756c5618a119544
Michael Permyashkin <mp2471@cs.ship.edu>
2021-03-27 13:56:18 -0700

    Start up commmands done

--- sush.c
+++ sush.c
@@ -17,54 +17,66 @@
 
 #define CMD_BUFFER 512 
 
+
+/**
+ * If user defined startup command in the .sushrc file and has permission
+ * to read and execute commands, the function executes each command
+ */ 
 void run_startup_commands() {
-    //Setup filename
-    
-}
-
-int main(int argc, char *argv[], char *envp[]) {
-    int rc;
-
-    //Environment Setup
-    environ_init(envp);
-
-    //Run startup commands
-    //run_startup_commands();
-
+    // build .sushrc filepath
     char *sushhome = environ_get_var("SUSHHOME")->value;
     char *filename = malloc(strlen(sushhome) + 9);
     strcpy(filename, sushhome);
     strcat(filename, "/.sushrc");
 
+    // get file permissions
     struct stat sfile;
     stat(filename, &sfile);
 
+    // if user can read and execute
     if ((sfile.st_mode & S_IRUSR) && (sfile.st_mode & S_IXUSR)) {
         FILE *fp = fopen(filename, "r");
 
+        // read and execute each line
         char cmdline[CMD_BUFFER];
-        while (fgets(cmdline, CMD_BUFFER - 1, fp) != NULL) {
-            //printf("STARTUP: %s\n", cmdline);
+        while (fgets(cmdline, CMD_BUFFER, fp) != NULL) {
             do_command(cmdline);
         }
 
+        //close file
         fclose(fp);
     }
+}
 
-    //Command Processing
+
+/**
+ * Launches the shell by first initializing environement, executing any
+ * startup command defined in .sushrc file and then enters loop to prompt
+ * user for command line input
+ */ 
+int main(int argc, char *argv[], char *envp[]) {
+    int rc;
+
+    // Environment Setup
+    environ_init(envp);
+
+    // Run startup commands
+    run_startup_commands();
+
+    // Command Processing
     char cmdline[CMD_BUFFER];
     while(1) {
         printf("%s ", environ_get_var("PS1")->value);
         fflush(stdout);
+
         if (fgets(cmdline, CMD_BUFFER-1, stdin) != NULL ) {
+
+            // if not empty command, execute
             if (cmdline[0] != '\n') {
                 rc = do_command(cmdline);
             }
         }
     }
-
-    //Clear environ
-    environ_clean_up();
 
     return 0;
 }
